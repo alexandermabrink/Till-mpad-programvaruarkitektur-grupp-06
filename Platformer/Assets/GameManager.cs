c83358c42c76b53eef3e870b3830fbaeed2b1b4a
Alexander Måbrink <45686166+alexandermabrink@users.noreply.github.com>
2020-05-18 15:35:23 +0200

    fixed sceneloader
    .

---
+++ Platformer/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FinishGame()
+    {
+
+    }
+
+    public void Death()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Start()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
--- Platformer/Assets/Scripts/FinishPoint.cs
+++ Platformer/Assets/Scripts/FinishPoint.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishPoint : MonoBehaviour
 {
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name.Equals("player"))
+        if(collision.gameObject.name.Equals("player") && SceneManager.GetActiveScene() != SceneManager.GetSceneByName("FinalLevel"))
         {
-            Debug.Log("Level Finish");
+            FindObjectOfType<GameManager>().LoadNextLevel();
+        } else
+        {
+            FindObjectOfType<GameManager>().FinishGame();
         }
     }
 }
