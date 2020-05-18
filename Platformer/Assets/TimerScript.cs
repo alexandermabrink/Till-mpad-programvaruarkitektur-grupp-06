d8125cfb9e42623499953ee12a6c3d0d32d2e09b
amir <amir.parmback@gmail.com>
2020-05-18 15:20:07 +0200

    Hej

---
+++ Platformer/Assets/TimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+
+
+{
+
+    public Text timerLabel;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        var minutes = time / 60;
+        var seconds = time % 60;
+        var fraction = (time * 100) % 100;
+
+        //update the label value
+        timerLabel.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
+    }
+}
