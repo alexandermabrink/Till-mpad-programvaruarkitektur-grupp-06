fffd5ef71a99519c54d0ee1ea8d3e08f87d11a2a
Alexander Måbrink <45686166+alexandermabrink@users.noreply.github.com>
2020-05-18 12:55:18 +0200

    First level
    Push up package for doing levels and the first level.

---
+++ Platformer/Assets/Scripts/FinishPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Equals("player"))
+        {
+            Debug.Log("Level Finish");
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/MaceFalling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaceFalling : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Equals("player"))
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("player"))
+        {
+            Debug.Log("HIT");
+        }
+    }
+
+   
+}
